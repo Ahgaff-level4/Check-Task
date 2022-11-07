f588c7c6b4c9585661d448427fe11574f4b16c0b
Ahmad-700 <81206551+Ahmad-700@users.noreply.github.com>
2022-11-07 14:31:28 +0300

    Update MainActivity.java

--- app/src/main/java/com/ahgaff_projects/mygoals/MainActivity.java
+++ app/src/main/java/com/ahgaff_projects/mygoals/MainActivity.java
@@ -21,7 +21,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-//welcome to Ahgaff-level4 org
+//welcome to Ahgaff-level4/MyGoals Repository
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
