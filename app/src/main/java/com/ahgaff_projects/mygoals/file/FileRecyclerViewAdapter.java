9a0b9ee5ea9e45c3d53a6cdfac520bc825f20070
Ahmad <ahmad.medoo.700@gmail.com>
2022-11-13 22:33:31 +0300

    update file > list > item > nearestReminder in each file

--- app/src/main/java/com/ahgaff_projects/mygoals/file/FileRecyclerViewAdapter.java
+++ app/src/main/java/com/ahgaff_projects/mygoals/file/FileRecyclerViewAdapter.java
@@ -15,10 +15,14 @@ import com.ahgaff_projects.mygoals.DATA;
 import com.ahgaff_projects.mygoals.R;
 import com.ahgaff_projects.mygoals.folder.Folder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class FileRecyclerViewAdapter extends RecyclerView.Adapter<FileRecyclerViewAdapter.ViewHolder> {
     private final FileListActivity context;
@@ -93,29 +97,35 @@ public class FileRecyclerViewAdapter extends RecyclerView.Adapter<FileRecyclerVi
     }
 
     private String nearestReminder(File thisFile) {
-        if (thisFile.getStartReminder() == null)
-            return "repeat:" + thisFile.getRepeatEvery();
-        LocalDateTime fromDateTime = LocalDateTime.now();
-        LocalDateTime toDateTime = thisFile.getStartReminder();
+        if (thisFile.getStartReminder() == null)//no startReminder means no repeatEvery because if user choose only repeatEvery then automatically startReminder will be set at that day
+            return "";
+        //get days different between now and startReminder (future)
+        Duration dur = Duration.between(LocalDateTime.now(), thisFile.getStartReminder());
+        long days = Math.round(((double) dur.toHours()) / 24)+1;//today days became -1 So add one. idk why
 
-        LocalDateTime tempDateTime = LocalDateTime.from(fromDateTime);
-//
-//        long years = tempDateTime.until( toDateTime, ChronoUnit.YEARS );
-//        tempDateTime = tempDateTime.plusYears( years );
-//
-//        long months = tempDateTime.until( toDateTime, ChronoUnit.MONTHS );
-//        tempDateTime = tempDateTime.plusMonths( months );
-
-        long days = tempDateTime.until(toDateTime, ChronoUnit.DAYS);
-//        tempDateTime = tempDateTime.plusDays( days );
-//        long hours = tempDateTime.until( toDateTime, ChronoUnit.HOURS );
-//        tempDateTime = tempDateTime.plusHours( hours );
-//
-//        long minutes = tempDateTime.until( toDateTime, ChronoUnit.MINUTES );
-//        tempDateTime = tempDateTime.plusMinutes( minutes );
-//
-//        long seconds = tempDateTime.until( toDateTime, ChronoUnit.SECONDS );
-
-        return "repeat:" + thisFile.getRepeatEvery() + " days:" + LocalDateTime.now().until(thisFile.getStartReminder(), ChronoUnit.DAYS);
+        if(days < 0){
+            //startReminder has pass and no repeatEvery!
+            if(thisFile.getRepeatEvery() == -1)
+                return "";
+            //app will reach here if startReminder=old date and repeatDays exist
+            //startReminder= old date.
+            //repeatDays= n days.
+            // So, remain days = (now date - old date) % repeatDays
+            days =(-days) % thisFile.getRepeatEvery();//old date is negative
+        }
+            switch ((int) days){
+                case 0: return context.getString(R.string.today);
+                case 1: return context.getString(R.string.tomorrow);
+                case 2: return context.getString(R.string.after_tomorrow);
+                case 3://empty case will fall to below case. So all empty cases will execute case 10 code.
+                case 4:// 10 ايام
+                case 5:// 11 يوم
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:return context.getString(R.string.after)+" "+days+" "+context.getString(R.string.days);
+                default:return context.getString(R.string.after)+" "+days+" "+context.getString(R.string.arabic_days);
+            }
     }
 }
